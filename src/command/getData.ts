b0ac7084e1b6f3ff731fd0c0c5d2a515d25d7727
Thomas Miller <thomas.d.miller.jr@gmail.com>
2020-06-29 00:00:45 -0700

    Add cli-table3

--- src/command/getData.ts
+++ src/command/getData.ts
@@ -1,10 +1,16 @@
 const axios = require('axios');
+const Table = require('cli-table3');
+
+const table = new Table({
+  head: ['Symbol', 'Value']
+})
 
 const getData = async (url: string) => {
   try {
     const response = await axios.get(url);
-    const data = response.data;
-    console.table(data.rates);
+    const data = response?.data;
+    Object.entries(data?.rates).forEach(c => table.push(c))
+    console.log(table.toString())
   } catch (error) {
     console.log(error);
   }
