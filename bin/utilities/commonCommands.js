acd15c279cc3e34bf898349edb47627a9d1b1368
Thomas Miller <thomas.d.miller.jr@gmail.com>
2020-06-30 18:02:29 -0700

    Create HOC to accept custom functions

--- bin/utilities/commonCommands.js
+++ bin/utilities/commonCommands.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var url = require('url');
 var getData_1 = __importDefault(require("../command/getData"));
 var CommonAttributes;
 (function (CommonAttributes) {
@@ -13,20 +12,18 @@ var CommonAttributes;
     CommonAttributes["places"] = "places";
     CommonAttributes["source"] = "source";
 })(CommonAttributes || (CommonAttributes = {}));
-var commonCommands = function (url) {
+var commonCommands = function (endpoint, f) {
+    var BASE_URL = new URL(endpoint);
+    var params = ['base', 'symbols', 'amount', 'places', 'source'];
     return function (program) {
-        var BASE_URL = new URL(url);
-        var params = ['base', 'symbols', 'amount', 'places', 'source'];
-        if (program.date) {
-            BASE_URL.pathname = program.date;
-        }
+        BASE_URL = f(program, BASE_URL);
         for (var _i = 0, params_1 = params; _i < params_1.length; _i++) {
             var p = params_1[_i];
             if (program[p]) {
                 BASE_URL.searchParams.append(p, program[p]);
             }
         }
-        getData_1.default(BASE_URL.href);
+        return getData_1.default(BASE_URL.href);
     };
 };
 exports.default = commonCommands;
--- src/utilities/commonCommands.ts
+++ src/utilities/commonCommands.ts
@@ -1,4 +1,3 @@
-const url = require('url');
 import getData from '../command/getData';
 
 enum CommonAttributes {
@@ -6,22 +5,17 @@ enum CommonAttributes {
   symbols = 'symbols',
   amount = 'amount',
   places = 'places',
-  source = 'source'
+  source = 'source',
 }
 
-type Program = {
-  [keyof in CommonAttributes]?: string;
-}
+export type Program<T> = Record<CommonAttributes, string> & T
 
-const commonCommands = (url: string) => {
-  return (program: any) => {
-    let BASE_URL = new URL(url);
+const commonCommands = <T>(endpoint: string, f: (program: Program<T>, queryUrl: URL) => URL) => {
+  let BASE_URL = new URL(endpoint);
+  const params: Array<keyof typeof CommonAttributes> = ['base', 'symbols', 'amount', 'places', 'source'];
 
-    const params: Array<keyof typeof CommonAttributes> = ['base', 'symbols', 'amount', 'places', 'source'];
-
-    if (program.date) {
-      BASE_URL.pathname = program.date;
-    }
+  return (program: Program<T>) => {
+    BASE_URL = f(program, BASE_URL)
 
     for (let p of params) {
       if (program[p]) {
@@ -29,7 +23,7 @@ const commonCommands = (url: string) => {
       }
     }
 
-    getData(BASE_URL.href)
+    return getData(BASE_URL.href)
   }
 }
 
